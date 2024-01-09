83864dfe27cae46a9c4dd54cee97c6133d664ae8
EronAlves1996 <eron.alves@rocketmail.com>
2024-01-09 01:14:10 -0300

    feat: setup initial screen

--- src/main/java/com/eronalves/simplemusicplayer/App.java
+++ src/main/java/com/eronalves/simplemusicplayer/App.java
@@ -1,30 +1,40 @@
 package com.eronalves.simplemusicplayer;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+  @Override
+  public void start (Stage stage) {
+    var label = new Label(
+        "Welcome to simple Music Player!!\nTo Start, please select a folder to scan musics!"
+    );
+    label.setAlignment(Pos.CENTER);
+    var button = new Button("Choose folder");
+    button.setOnMouseClicked(e -> System.out.println("Clicked"));
+    FlowPane rootElement =
+        new FlowPane(Orientation.VERTICAL, 5.0, 5.0, label, button);
+    rootElement.setColumnHalignment(HPos.CENTER);
+    rootElement.setAlignment(Pos.CENTER);
+    var scene = new Scene(rootElement, 640, 480);
+    stage.setScene(scene);
+    stage.setTitle("Simple music player");
+    stage.show();
+  }
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main (String[] args) {
+    launch();
+  }
 
 }
